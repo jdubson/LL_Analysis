6f6a3496c2d24c885994b588c6f365996eefe403
jdubson <jasonhsuwu@gmail.com>
2016-05-26 16:58:24 -0700

    Load sample data

---
+++ LL_Analysis.R
@@ -0,0 +1,10 @@
+# Create model using sample data
+# Author: Jason Wu
+
+# Load libraries
+library(tidyr)
+library(dplyr)
+
+
+# Import sample data
+ll_data <- read.csv("~/Desktop/LL_data.csv", header=TRUE, sep=",")
