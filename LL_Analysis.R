118e668bddbab83b85079dabf8b7d0491bc5ba7b
jdubson <jasonhsuwu@gmail.com>
2016-05-26 22:51:08 -0700

    Clean up inf and na summary values.

--- LL_Analysis.R
+++ LL_Analysis.R
@@ -23,11 +23,13 @@ ll_data_clean <- ll_data_clean %>%
   mutate(Revenue = Paid.Subs * ARPU, Trial.New = round(Trial.Temp/Trial.Conversion,0)) %>%
   select(Date, Trial.Conversion, Trial.New, Trial.Adds, Trial.Subs, Paid.Adds, Paid.Subs, Revenue)
 
+ll_data_clean$Trial.New <- as.numeric(gsub("Inf", NA, ll_data_clean$Trial.New))
+
 # Summarize into monthly data frame
 ll_data_monthly <- ll_data_clean
 ll_data_monthly$Month <- as.Date(as.yearmon(ll_data_monthly$Date))
 
 ll_data_monthly_summary <- ll_data_monthly %>%
-  group_by(month) %>%
-  summarise(avg_trial_conv = median(Trial.Conversion), tot_trial_new = sum(Trial.New), tot_trial_adds = sum(Trial.Adds),
+  group_by(Month) %>%
+  summarise(avg_trial_conv = median(Trial.Conversion), tot_trial_new = sum(Trial.New, na.rm = TRUE), tot_trial_adds = sum(Trial.Adds),
             tot_trial_subs = sum(Trial.Subs), tot_paid_adds = sum(Paid.Adds), tot_paid_subs = sum(Paid.Subs), tot_revenue = sum(Revenue))
